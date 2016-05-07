9f35196a7542181f454180a21a912b25f170cad4
Amy-Kate Andrews <amy-kate.andrews@wearetopsecret.com>
2016-05-07 16:06:35 -0600

    Adding files

---
+++ test/main_test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+// Test suite
+
+// sanity check
+describe('Mocha', function() {
+	// Test spec (unit test)
+	it('should run our tests using npm', function() {
+		expect(true).to.be.ok;
+	});
+});
